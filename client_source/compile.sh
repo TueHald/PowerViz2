84f3f21000689895285d7e2b5bb919fe51c03110
mkni1408 <mkni1408@gmail.com>
2014-05-18 11:23:31 +0200

    deleted compile

--- client_source/compile.sh
+++
@@ -1,8 +0,0 @@
-#The "compile" script for the PowerViz2 application.
-tsc --out ../client/Main.js --target ES5 Main.ts
-cp index.html ../client/index.html
-cp mainstyle.css ../client/mainstyle.css
-cp -r Fonts/ ../client/Fonts/
-cp -r Images/ ../client/Images/
-sh upload_dev_client.sh
-
